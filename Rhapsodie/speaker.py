33e4d6fe3a8268c27bb1af7c1d36857bf1ee854c
VanessaGB <81186046+VanessaGB@users.noreply.github.com>
2021-05-26 13:20:04 +0200

    Update speaker.py

--- Rhapsodie/speaker.py
+++ Rhapsodie/speaker.py
@@ -8,20 +8,6 @@ le // pouvant symboliser aussi bien le passage à une autre phrase que celui à 
 La solution semblerait être de se servir de ce script pour une première annotation et d'ensuite se référer aux enregistrements pour éventuellement la corriger.
 """
 
-# with open('fr_spoken.sud.dev.conllu.txt','r',encoding="utf8") as fichier:
-#     sortie=open('fr_spoken.sud.dev.conllu_v2.txt','w',encoding="utf8")
-#     speaker="L1"
-#     for ligne in fichier:
-#         sortie.write(ligne)
-#         if re.match('# macrosyntax =',ligne):
-#             l1,l2=ligne.split(" = ")
-#             l2=re.sub(r"\-\$|\$\-|\(.*\)","",l2)
-#             #sortie.write(l2)
-#             if re.match('\$L',l2):
-#                 speaker=l2[1:3]
-#             sortie.write(f"# speaker = {speaker}\n")
-# Traiter les lignes où il y a déjà un speaker - passer par les indices ?
-
 with open('fr_spoken.sud.dev.conllu.txt','r',encoding="utf8") as fichier:
     fichier=fichier.readlines()
     sortie=open('fr_spoken.sud.dev.conllu_v2.txt','w',encoding="utf8")
@@ -30,12 +16,26 @@ with open('fr_spoken.sud.dev.conllu.txt','r',encoding="utf8") as fichier:
         sortie.write(fichier[ligne])
         if re.match('# macrosyntax =',fichier[ligne]):
             l1,l2=fichier[ligne].split(" = ")
-            #print(l2)
             l2=re.sub(r"\-\$ |\$\- |\(.*\)","",l2)
-            #sortie.write(l2)
             if not re.match('# speaker',fichier[ligne+1]):
                 if re.match('\$L',l2):
                     speaker=l2[1:3]
                 sortie.write(f"# speaker = {speaker}\n")
-            # else:
-            #     sortie.write("déjà là - ")
+                
+# Ci-dessous, une autre version tenant compte des lignes où le locuteur est déjà donné pour récupérer son id.
+# with open('fr_spoken.sud.test.conllu.txt','r',encoding="utf8") as fichier:
+#     fichier=fichier.readlines()
+#     sortie=open('fr_spoken.sud.test.conllu_v2.txt','w',encoding="utf8")
+#     speaker="L1"
+#     for ligne in range(len(fichier)):
+#         sortie.write(fichier[ligne])
+#         if re.match('# macrosyntax =',fichier[ligne]):
+#             l1,l2=fichier[ligne].split(" = ")
+#             l2=re.sub(r"\-\$ |\$\- |\(.*\)","",l2)
+#             if not re.match('# speaker',fichier[ligne+1]):
+#                 if re.match('\$L',l2):
+#                     speaker=l2[1:3]
+#                 sortie.write(f"# speaker = {speaker}\n")
+#             else:
+#                 l1,l2=fichier[ligne+1].split(" = ")
+#                 speaker=l2[0:len(l2)-1]
